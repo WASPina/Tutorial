677162319424544e4137918fcb1dc93e85074eea
WASP <WASP@WASP-PC>
2016-03-26 18:32:53 +0200

    new project

---
+++ Learning/src/ua/org/wasp/practice/Program.java
@@ -0,0 +1,19 @@
+package ua.org.wasp.practice;
+
+public class Program {
+	private Deal[] deals;
+	public Deal input(){
+		String bayerName = keyboard();
+		Party bayer = new Party();
+		bayer.setName(bayerName);
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
