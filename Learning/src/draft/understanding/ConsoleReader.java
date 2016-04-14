618c7dcdba05e541cfca50bc1a23c7f1f63ede6a
WASP <WASP@WASP-PC>
2016-04-14 18:47:10 +0300

    expansion of task

--- Learning/src/draft/understanding/ConsoleReader.java
+++ Learning/src/draft/understanding/ConsoleReader.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleReader {
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	static ConsoleReader consoleReader = new ConsoleReader();
 
-	public String readInput() throws IOException {
+	private String readInput() throws IOException {
 		String buff = reader.readLine();
 		return buff;
 	}
