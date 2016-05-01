fc14de6e93428fc928bf57079fd08b6174a294f6
WASP <WASP@WASP-PC>
2016-05-01 07:32:44 +0300

    correct path

---
+++ Learning/src/files/list/Main.java
@@ -0,0 +1,13 @@
+package files.list;
+
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		private String dirName = "f:\\\\DOCs and Manuals\\\\Books";
+	File f = new File ("f:\\\\DOCs and Manuals\\\\Books");
+
+	}
+
+}
